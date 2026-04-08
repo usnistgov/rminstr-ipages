133f7ef51ad95edc4727928a7c21f62f8093210e
d-c-gray <daniel.c.gray@nist.gov>
2026-04-08 16:05:05 +0000

    updating docs - automated

--- stable/_static/documentation_options.js
+++ stable/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.6.6a3.dev2+ga2c84b576.d20260407',
+    VERSION: '0.6.6a3.dev2+ga2c84b576.d20260408',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
